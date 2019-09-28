dd1015b2b948d0fd524105eaceff2b97fafbcc86
shernk <31926953+shernk@users.noreply.github.com>
2019-09-28 18:27:55 +0700

    install for package.json

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
-
 const app = express();
 
 // Bodyparser Middleware
